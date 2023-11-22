d17e2156765b13669e070dda90f9acb052837257
S. Mehran Abghari <mehran.ab80@gmail.com>
2023-11-22 16:56:15 +0300

    updated driver test

--- test/driver.ts
+++ test/driver.ts
@@ -3,7 +3,11 @@ import { getHeadlessDriver } from '../src/scrappers/driver'
 const run = async () => {
   const driver = await getHeadlessDriver()
   await driver.get('https://digikala.com')
-  await driver.sleep(5000)
+  await driver.sleep(1000)
+
+  const title = await driver.getTitle()
+  console.log(title)
+
   await driver.close()
 }
 
